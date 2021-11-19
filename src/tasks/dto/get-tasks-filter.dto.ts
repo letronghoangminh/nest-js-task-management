c64d29cd4fa894bf0db7d2e302805b8dae4f49da
Le Trong Hoang Minh <letronghoangminh@gmail.com>
2021-11-19 08:54:44 +0700

    Section 3

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 
 export class GetTasksFilterDto {
+  @IsEnum(TaskStatus)
+  @IsOptional()
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -32,7 +32,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException('Nothing found with that ID');
+    }
+
+    return task;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -50,6 +56,7 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
+    this.getTaskById(id); // Calling this function for handling not found id
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
